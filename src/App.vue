b447a0edfc9826dbd315af1ca5864e95cc4fdf47
kaynanDev <kaynan2817@gmail.com>
2025-03-13 13:20:38 -0300

    criação e estilização do header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script setup>
-
+import Data from './components/Data.vue';
 </script>
 
 <template>
-  
+  <div class="container">
+    <Data />
+  </div>
 </template>
 
-<style scoped>
+<style >
+ body{
+  margin: 0;
+  max-width: 1220px;
+  width: 100%;
+  height: 100vh;
+  padding: 0 24px;
+  box-sizing: border-box;
+  background-color: hsl(0, 0%, 94%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+ }
+
  
 </style>
